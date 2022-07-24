60966fcbef04f18916c83457bca6569c9d0d17fa
Robson Natanael <natanaelrobson@gmail.com>
2022-07-23 22:49:20 -0300

    chore: install styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from "./components/Buttons";
+
 export function App() {
-  return <h1>Ignite Timer</h1>;
+  return (
+    <>
+      <h1>Ignite Timer</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
 }
