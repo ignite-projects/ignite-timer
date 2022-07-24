965670cda59c059cfe467af9599d50bc002483a2
Robson Natanael <natanaelrobson@gmail.com>
2022-07-24 13:24:34 -0300

    feat: configure theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Buttons";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Ignite Timer</h1>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+    </ThemeProvider>
   );
 }
