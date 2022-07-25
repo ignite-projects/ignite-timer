248061f84d52fe54f101baafbcd42f3370ede31c
Robson Natanael <natanaelrobson@gmail.com>
2022-07-24 21:00:12 -0300

    refactor: remove component obsolete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Buttons'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer</h1>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
       <GlobalStyle />
     </ThemeProvider>
   )
