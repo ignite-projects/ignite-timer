b2fca49c5fd0bbf1630f9ea570ac4d97e0bdb003
Robson Natanael <natanaelrobson@gmail.com>
2022-08-02 17:00:41 -0300

    fix: clear countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,13 +47,19 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: any
+
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           // eslint-disable-next-line prettier/prettier
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -67,6 +73,7 @@ export function Home() {
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     reset()
   }
 
